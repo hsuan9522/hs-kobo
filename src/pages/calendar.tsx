3c5aeaf800b8d49453c5b1fcb34b008f91803444
hsuan9522 <jh558125@gmail.com>
2025-04-25 17:28:49 +0800

    [Feat] 增加 sqljs、input

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -2,9 +2,13 @@ import FullCalendar from '@fullcalendar/react'
 import { EventContentArg } from '@fullcalendar/core/index.js'
 import multiMonthPlugin from '@fullcalendar/multimonth'
 import dayjs from 'dayjs'
-import { Flex, Text } from '@chakra-ui/react'
+import { Flex, Text, Group, Input, Button, Box, Icon } from '@chakra-ui/react'
+import { useState } from 'react'
+import { LuX } from 'react-icons/lu'
 
 const Calendar = () => {
+    const [value, setValue] = useState('')
+
     const events = [
         {
             title: '1到時候會是中文字啊',
@@ -50,12 +54,40 @@ const Calendar = () => {
         <Flex
             direction="column"
             align="center"
-            justify="center"
-            py="2"
+            justify="start"
+            py="4"
+            gap="8"
             height="90%"
             px={{ md: '20' }}
             mx={{ md: '20' }}
         >
+            <Box position="relative" width={{ md: '60%', base: '100%' }}>
+                {value && (
+                    <Icon
+                        position="absolute"
+                        right="84px"
+                        top="50%"
+                        translate="0 -50%"
+                        color="gray.400"
+                        zIndex="1"
+                        onClick={() => setValue('')}
+                    >
+                        <LuX />
+                    </Icon>
+                )}
+                <Group attached w="full">
+                    <Input
+                        value={value}
+                        flex="1"
+                        borderColor="gray.300"
+                        placeholder="Enter your url"
+                        onChange={(e) => {
+                            setValue(e.currentTarget.value)
+                        }}
+                    />
+                    <Button>Submit</Button>
+                </Group>
+            </Box>
             <FullCalendar
                 height={'100%'}
                 aspectRatio={1.25}
